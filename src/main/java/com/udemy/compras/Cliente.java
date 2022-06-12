0e3e6c2828745a6cae87f9785dc3e5d438da1d0b
renatograca <renatomacedo1995@gmail.com>
2022-06-12 20:12:07 -0300

    add get one and get all

---
+++ src/main/java/com/udemy/compras/Cliente.java
@@ -0,0 +1,11 @@
+package com.udemy.compras;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cliente {
+    private String nome;
+    private  String email;
+}
--- src/main/java/com/udemy/compras/QueryGraphql.java
+++ src/main/java/com/udemy/compras/QueryGraphql.java
@@ -3,6 +3,9 @@ package com.udemy.compras;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class QueryGraphql implements GraphQLQueryResolver {
 
@@ -11,5 +14,17 @@ public class QueryGraphql implements GraphQLQueryResolver {
     }
     public int soma(int a, int b ) {
         return a + b ;
+    }
+
+    public Cliente cliente() {
+        return new Cliente("Renato", "email");
+    }
+
+    public List<Cliente> clientes() {
+        List<Cliente> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(new Cliente("cliente " +i, "r"+i+"@email.com"));
+        }
+                return list;
     }
 }
