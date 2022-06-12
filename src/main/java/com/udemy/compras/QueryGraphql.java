c4d478a2f24d5ff6e6730ecb70ba90813cef34d4
renatograca <renatomacedo1995@gmail.com>
2022-06-12 19:53:43 -0300

    initial graphql

---
+++ src/main/java/com/udemy/compras/QueryGraphql.java
@@ -0,0 +1,15 @@
+package com.udemy.compras;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QueryGraphql implements GraphQLQueryResolver {
+
+    public String hello() {
+        return "Hello Graphql";
+    }
+    public int soma(int a, int b ) {
+        return a + b ;
+    }
+}
